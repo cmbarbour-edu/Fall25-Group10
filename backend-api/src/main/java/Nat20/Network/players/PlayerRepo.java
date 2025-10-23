c54178ddfb1be42ee8b9ce30b72a1a0ea5e42fd8
Brycen5 <bmparsons@uncg.edu>
2025-10-23 13:50:23 -0400

    Finished player repo

--- backend-api/src/main/java/Nat20/Network/players/PlayerRepo.java
+++ backend-api/src/main/java/Nat20/Network/players/PlayerRepo.java
@@ -2,12 +2,11 @@ package Nat20.Network.players;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
-import Nat20.Network.players.*;
+
 
 @Repository
-public class PlayerRepo extends JpaRepository<Player, Long>{
-    
+public interface PlayerRepo extends JpaRepository<Player, Long>{
+    boolean existsByEmail(String email);
+    Optional<Player> findByEmail(String email);
 }
