26d7dfb8651560f6362d3cf5803489140ca0f361
Brycen5 <bmparsons@uncg.edu>
2025-10-24 14:06:08 -0400

    Created Template for Requests.java

--- backend-api/src/main/java/Nat20/Network/Requests/Requests.java
+++ backend-api/src/main/java/Nat20/Network/Requests/Requests.java
@@ -1,5 +1,43 @@
 package Nat20.Network.requests;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import Nat20_Network.players.Player;
+import Nat20_Network.campaigns.Campaign;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "requests")
+
 public class Requests {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "player_id", nullable = false)
+    @JsonIgnoreProperties("requests")
+    private Player player;
+
+    @ManyToOne
+    @JoinColumn(name = "campaign_id", nullable = false)
+    @JsonIgnoreProperties({"requests"})
+    private Campaign campaign;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private SubscriptionType type;
+
+    @NotNull
+    private boolean active = true;
 }
+
+enum SubscriptionType {
+    ONE_TIME 
+}
+
