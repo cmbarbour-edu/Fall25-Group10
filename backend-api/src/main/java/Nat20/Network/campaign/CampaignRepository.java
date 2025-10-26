2d79292bc3d12e7d978852ee5b24ab17f7e5c9b6
nemofasho <nehemiahmcclain11@gmail.com>
2025-10-26 19:24:16 -0400

    accidental fix

--- backend-api/src/main/java/Nat20/Network/campaign/CampaignRepository.java
+++ backend-api/src/main/java/Nat20/Network/campaign/CampaignRepository.java
@@ -6,12 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import Nat20.Network.dungeonMaster.DM;
 
 @Repository
 public interface CampaignRepository extends JpaRepository<Campaign, Long>{
     @Query(value = "select * from campaigns c where c.title like %?1%", nativeQuery = true)
-    List<Campaign> findByTitle(DM dmId);
+    List<Campaign> findByTitle(String title);
 
     @Query(value = "select * from campaigns c where c.edition like %?1%", nativeQuery = true)
     List<Campaign> findByEdition(String edition);
