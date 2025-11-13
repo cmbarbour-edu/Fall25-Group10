3a84760eb9e24aa31e3f50a86056e6102a78347c
cbarbour-edu <cmbarbour@uncg.edu>
2025-11-13 12:08:27 -0500

    Updated dependencies to include MVC controller. Changed DM and campaign controllers to use the updated MVC controller instead of the Rest controller.

--- mvc-app/src/main/java/Nat20/Network/campaign/CampaignController.java
+++ mvc-app/src/main/java/Nat20/Network/campaign/CampaignController.java
@@ -1,44 +1,64 @@
 package Nat20.Network.campaign;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/campaigns")
+@Controller
+@RequestMapping("/campaigns")
 @RequiredArgsConstructor
 public class CampaignController {
     private final CampaignService campaignService;
     
-    @PostMapping
-    public ResponseEntity<Campaign> createCampaign(@Valid @RequestBody Campaign campaign) {
-        return ResponseEntity.ok(campaignService.createCampaign(campaign));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Campaign> updateCampaign(@PathVariable Long id, @Valid @RequestBody Campaign campaignDetails) {
-        return ResponseEntity.ok(campaignService.updateCampaign(id, campaignDetails));
-    }
-
+    // Campaign forms
     @GetMapping
-    public ResponseEntity<Object> getAllCampaigns() {
-        return ResponseEntity.ok(campaignService.getAllCampaigns());
+    public Object viewAllCampaigns(Model model) {
+        model.addAttribute("campaigns", campaignService.getAllCampaigns());
+        model.addAttribute("title", "Campaigns:");
+        return "campaign-list";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Campaign> getCampaign(@PathVariable Long id) {
-        return ResponseEntity.ok(campaignService.getCampaignById(id));
+    @GetMapping("/createForm")
+    public Object showCreateCampaignForm(Model model) {
+        Campaign campaign = new Campaign();
+        model.addAttribute("campaign", campaign);
+        model.addAttribute("title", "Add a new campaign");
+        return "campaign-create";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCampaign(@PathVariable Long id) {
-        campaignService.deleteCampaign(id);
-        return ResponseEntity.noContent().build();
+    @PostMapping
+    public Object addCampaign(Campaign campaign) {
+        Campaign newCampaign = campaignService.createCampaign(campaign);
+        return "redirect:/campaigns/" + newCampaign.getCampaignID(); 
     }
 
+    @GetMapping("/updateForm/{campaignID}")
+    public Object showUpdateForm(@PathVariable Long campaignID, Model model) {
+        Campaign campaign = campaignService.getCampaignById(campaignID);
+        model.addAttribute("Campaign", campaign);
+        model.addAttribute("title", "Update Campaign");
+        return "campaign-update";
+    }
+
+    @PostMapping("/{campaignID}/update")
+    public Object updateCampaign(@PathVariable Long campaignID, Campaign campaignDetails) {
+        campaignService.updateCampaign(campaignID, campaignDetails);
+        return "redirect:/campaigns/" + campaignID;
+    }
+
+    @GetMapping("/{campaignID}")
+    public Object getCampaignByID(@PathVariable Long campaignID, Model model) {
+        model.addAttribute("Campaign", campaignService.getCampaignById(campaignID));
+        model.addAttribute("title", campaignService.getCampaignById(campaignID).getTitle());
+        return "campaign-details";
+    }
+
+    @GetMapping("/{campaignID}/delete")
+    public Object deleteDm(@PathVariable Long campaignID) {
+        campaignService.deleteCampaign(campaignID);
+        return "redirect:/home";
+    }
 
 }
